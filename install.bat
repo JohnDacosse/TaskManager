6decb99aa5b1d2581404a2bdbc94a6fb62e2a6ff
JohnDacosse <john.dacosse@protonmail.com>
2021-07-16 14:41:41 +0200

    batch search

--- install.bat
+++ install.bat
@@ -9,10 +9,6 @@ python manage.py migrate
 
 echo "## Création de l'administrateur DB"
 python manage.py createsuperuser
-from .models import *
 
-
-user1 = User.objects.create_user('john', 'john.technicien@email.be', 'azerty')
-user2 = User.objects.create_user('megan', 'megan.technicien@email.be', 'qwerty')
-user1.save()
-user2.save()
+echo "## Insertions DataBase"
+python manage.py shell < ../set_database.txt
